f47b383226a26e48062a80559973ebd3b7f2491e
Fox.Lee <jiaqing.li@outlook.com>
2012-09-14 16:56:23 +0800

    Chapter 2
    import

---
+++ 02-list-movie-function.py
@@ -0,0 +1,7 @@
+from nester import print_lol 
+movies = [
+     'The Holy Grail', 1975, 'Terry Jones&Terry Gilliam', 91,
+     ['Graham Chapman',
+      ['Michael Palin', 'John Cleese','Terry Gilliam','Eric Idle','Terry Jones']]]
+
+print_lol(movies)

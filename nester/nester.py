2f5d75ae90fb7a1f5a3397a94677abc15b05eb4e
Fox.Lee <jiaqingli@gmail.com>
2012-09-14 23:22:46 +0800

    change the nester
    level 可以让各个子的 list 缩进,每个 list 缩进一个tab

--- nester/nester.py
+++ nester/nester.py
@@ -1,12 +1,17 @@
 """
 This can print all the intem in the list or in the list of list
 """
-def print_lol(the_list):
+def print_lol(the_list,level):
      """
-     Input a list 
+     method 2, printtab is a bool value, if it is ture,in each sub list, it will print
+     a TAB 
      """
      for each_item in the_list:
           if (isinstance(each_item,list)):
-               print_lol(each_item) #这个递归函数真不赖
+               print_lol(each_item,level+1)
           else:
+               for tab_stop in range (level):
+                    print('\t',end='')
                print(each_item)
+
+      
--- nester/setup.py
+++ nester/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
      name         = 'nester',
-     version      = '1.0.0',
+     version      = '1.1.0',
      py_modules   = ['nester'],
      author       = 'Fox',
      author_email ='a@b.com',

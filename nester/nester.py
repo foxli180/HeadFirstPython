132ac5e7258c4a2f03cb158ad404642693d0a825
Fox.Lee <jiaqingli@gmail.com>
2012-09-14 23:38:58 +0800

    增加indent 可以自由选择是否缩进
    旧版本默认不缩进

--- nester/nester.py
+++ nester/nester.py
@@ -1,17 +1,21 @@
 """
 This can print all the intem in the list or in the list of list
 """
-def print_lol(the_list,level=0):
+def print_lol(the_list,indent=False,level=0):
      """
      method 2, printtab is a bool value, if it is ture,in each sub list, it will print
-     a TAB 
+     a TAB
+     the_list: type:list
+     indent: if this true, will print tab
+     level: 缩进的起始量,负数的话不缩进
      """
      for each_item in the_list:
           if (isinstance(each_item,list)):
                print_lol(each_item,level+1)
           else:
-               for tab_stop in range (level):
-                    print('\t',end='')
+               if indent:
+                    for tab_stop in range (level):
+                         print('\t',end='')
                print(each_item)
 
       
--- nester/setup.py
+++ nester/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup (
      name         = 'nester',
-     version      = '1.2.0',
+     version      = '1.3.0',
      py_modules   = ['nester'],
      author       = 'Fox',
      author_email ='a@b.com',

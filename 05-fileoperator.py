77258e6d018025ffadf173211ad5fb2ab7240db8
Fox.Lee <jiaqing.li@outlook.com>
2012-09-19 14:57:17 +0800

    使用异捕捉来处理例外情况

--- 05-fileoperator.py
+++ 05-fileoperator.py
@@ -19,4 +19,18 @@ for each_line in data:
           print (each_line, end='')
 
      
+print('\n------------------------------------')
+data.seek(0)
+
+for each_line in data:
+     try:
+          (role, line_spoken) = each_line.split (':',1)
+          print (role ,end='')
+          print (' said: ', end='')
+          print (line_spoken, end='')
+     except:
+          #pass
+          print (each_line, end='')
+
+
 data.close()

8d8378428185a0251bec5ce928230024d49f0e29
Fox.Lee <jiaqing.li@outlook.com>
2012-09-19 15:01:19 +0800

    异常捕捉

--- 05-fileoperator.py
+++ 05-fileoperator.py
@@ -23,13 +23,13 @@ print('\n------------------------------------')
 data.seek(0)
 
 for each_line in data:
-     try:
+     try: # try to split each_line, if get exception print the whole line
           (role, line_spoken) = each_line.split (':',1)
           print (role ,end='')
           print (' said: ', end='')
           print (line_spoken, end='')
      except:
-          #pass
+          #pass # pass: you can pass the exception
           print (each_line, end='')
 
 

7b52444e93297a8da8b8e866b304dae2e87a5bf6
Fox.Lee <jiaqing.li@outlook.com>
2012-09-19 14:34:01 +0800

    一行内出现过个分隔符会报错

--- 05-fileoperator.py
+++ 05-fileoperator.py
@@ -1,9 +1,19 @@
 import os
 
 print (os.getcwd())
-data = open('sketch.txt.txt')
+data = open('sketch.txt')
 print (data.readline(), end='')
-data.seek(0)
+data.seek(0) #Go back to the 1st line
 for each_line in data:
      print(each_line, end='')
+
+print('\n------------------------------------')
+data.seek(0) #Go back to the 1st line
+for each_line in data:
+     (role, line_spoken) = each_line.split(':')
+     print (role ,end='')
+     print (' said: ', end='')
+     print (line_spoken, end='')
+
+     
 data.close()

1bcef1f30b362cddf1de8d8bedc09cbe00f42ceb
Fox.Lee <jiaqing.li@outlook.com>
2012-09-19 14:38:35 +0800

    出现了不含分割符的行,则直接打印出来

--- 05-fileoperator.py
+++ 05-fileoperator.py
@@ -10,10 +10,13 @@ for each_line in data:
 print('\n------------------------------------')
 data.seek(0) #Go back to the 1st line
 for each_line in data:
-     (role, line_spoken) = each_line.split(':',1)
-     print (role ,end='')
-     print (' said: ', end='')
-     print (line_spoken, end='')
+     if each_line.find(':') > 0:
+          (role, line_spoken) = each_line.split(':',1)
+          print (role ,end='')
+          print (' said: ', end='')
+          print (line_spoken, end='')
+     else:
+          print (each_line, end='')
 
      
 data.close()

135641bb06d26912b849c539fb2ce76e9d911ac8
Fox.Lee <jiaqing.li@outlook.com>
2012-09-19 15:11:16 +0800

    使用 if 来处理文件是否能打开

--- 05-fileoperator.py
+++ 05-fileoperator.py
@@ -1,6 +1,21 @@
 import os
 
-print (os.getcwd())
+
+if os.path.exists('sketch.txt'):
+     data = open ('sketch.txt')
+
+     for each_line in data:
+          if each_line.find(':')>0:
+               (role,line_spoken) = each_line.split(':',1)
+               print(role ,end ='')
+               print(' said: ',end ='')
+               print(line_spoken,end='')
+          else:
+               print (each_line,end='')
+     data.close()
+     
+
+print ('\n'+os.getcwd())
 data = open('sketch.txt')
 print (data.readline(), end='')
 data.seek(0) #Go back to the 1st line

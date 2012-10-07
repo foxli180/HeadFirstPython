b09e2c2e916810e9e7cefe0ee474d8a8a2433fb1
Fox.Lee <jiaqingli@gmail.com>
2012-10-07 20:43:18 +0800

    finally的使用

--- 06-fileoperator2.py
+++ 06-fileoperator2.py
@@ -26,12 +26,15 @@ try: # 将 man 和other 分别写入文件
     out_man = open('man_data.txt','w')
     out_other = open('other_data.txt','w')
     print(man,file=out_man)
-    print(other,file=out_other)
-    out_man.close()
-    out_other.close()
+    print(other,file=out_other) #注意,如果这里出现了异常的话,文件无法正常关闭!引入 finally
+    #out_man.close() 
+    #out_other.close()
 except IOError:
     print('Can not write to file')
-    
+
+finally:#确保无论发生了什么都关文件,无论是否有运行时错误
+    out_man.close()
+    out_other.close()
 
     
             

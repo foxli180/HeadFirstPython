162255299cd25e96896cfa5a57146dce469ccf3b
Fox.Lee <jiaqingli@gmail.com>
2012-10-07 20:33:25 +0800

    add comments

--- 06-fileoperator2.py
+++ 06-fileoperator2.py
@@ -7,18 +7,18 @@
 man = []
 other = []
 try:
-    data = open('sketch.txt')
-    for each_line in data:
+    data = open('sketch.txt') #打开文件
+    for each_line in data:#遍历文件
         try:
-            (role,line_spoken) = each_line.split(':',1)
-            line_spoken = line_spoken.strip()
-            if role == 'Man':
+            (role,line_spoken) = each_line.split(':',1) #将每行按照:分成2部分
+            line_spoken = line_spoken.strip()#去除多余空格
+            if role == 'Man':#如果role是man, 把line_spken 压入man list
                 man.append(line_spoken)
             elif role == 'Other Man':
                 other.append(line_spoken)
         except ValueError:
             pass
-    data.close()
+    data.close()#关闭I/O
 except IOError:
     print('The data file is missing!')
 print(man)

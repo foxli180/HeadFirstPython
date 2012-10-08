47c4a6d769c0faac15e3874f4010d56f2f8bf3ee
Fox.Lee <jiaqing.li@outlook.com>
2012-10-08 16:48:17 +0800

    输入文件修改了,加入了名字和出生日期

--- 09-filereader.py
+++ 09-filereader.py
@@ -7,9 +7,11 @@ except IOError as err:
     print('file error: '+str(err))
 
 print('\n')
+
 try:
-    with open('man_data3.txt') as mdf:
-        data=mdf.readline()
+    
+    with open('sarah2.txt') as mdf:
+        data=mdf.readlines()
         print(data)
 except IOError as err:
     print('file error: '+str(err))
---
+++ 13-p175.py
@@ -0,0 +1,36 @@
+def sanitize(time_string): #序列化得到的无规则数据,将其格式化为 mins.secs 的格式
+     if '-' in time_string:
+          splitter = '-'
+     elif ':' in time_string:
+          splitter = ':'
+     else:
+          return(time_string)
+     (mins,secs)=time_string.split(splitter)
+     return(mins+'.'+secs)
+
+def load_from_file(filename): #读取文件的一行(这个文件只有一行,多行咋办)
+     try:
+          with open (filename) as f:
+               data = f.readline()
+               return (data.strip().split(','))
+     except IOError as err:
+          print('File Error: '+str(err))
+          return (None)
+
+#james = load_from_file('james.txt')
+#julie = load_from_file('julie.txt')
+#mikey = load_from_file('mikey.txt')
+sarah = load_from_file('sarah2.txt')
+
+
+
+#列表推导: 先将james 里的每个t格式化为统一的时间,再使用set()剔除重复值,再使用sort进行排序
+#使用[0:3]获得列表的前三位
+#print(sorted(set([sanitize(t) for t in james]))[0:3])
+#print(sorted(set([sanitize(t) for t in julie]))[0:3])
+#print(sorted(set([sanitize(t) for t in mikey]))[0:3])
+#print(sorted(set([sanitize(t) for t in sarah]))[0:3])
+
+
+(sarah_name,sarah_dob) = (sarah.pop(0),sarah.pop(0))
+print(sarah_name+"'s fastest times are: "+str(sorted(set([sanitize(t) for t in sarah]))[0:3]))
